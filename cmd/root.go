4ef80cdd4872bf7333d2a6d2aec0252864a38921
karnowsa <karnowsa@mailbox.org>
2020-06-29 22:20:47 +0200

    Added deployments command

--- cmd/root.go
+++ cmd/root.go
@@ -29,13 +29,11 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gologic",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Gologic is a CLI for the Oracle Weblogic Server",
+	Long: `The Oracle Weblogic has a RESTful Management Interface to do a lot of administration tasks.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+This is where gologictl comes in. With that tool you can to the tasks
+from the commandline and don't have to use the Weblogic Web Console.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
